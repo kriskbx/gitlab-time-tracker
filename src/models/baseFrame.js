09b17ea426e72a1deb76b703669b44e523ebbb87
kriskbx <m@kris.cool>
2018-08-03 11:18:43 +0200

    Refactor frame into baseFrame without fs dependency and frame with fs dependency

---
+++ src/models/baseFrame.js
@@ -0,0 +1,84 @@
+const moment = require('moment-timezone');
+const Hashids = require('hashids');
+const hashids = new Hashids();
+
+class baseFrame {
+    /**
+     * constructor.
+     * @param config
+     * @param id
+     * @param type
+     */
+    constructor(config, id, type) {
+        this.config = config;
+        this.project = config.get('project');
+        this.resource = {id, type};
+
+        if(typeof id === 'string' || id instanceof String)
+            this.resource.new = true;
+
+        this.id = baseFrame.generateId();
+        this._start = false;
+        this._stop = false;
+        this.timezone = config.get('timezone');
+        this.notes = [];
+    }
+
+    static fromJson(config, json) {
+        let frame = new this(config, json.resource.id, json.resource.type);
+        frame.project = json.project;
+        frame.id = json.id;
+        frame._start = json.start;
+        frame._stop = json.stop;
+        frame.notes = json.notes;
+        frame.timezone = json.timezone;
+        frame.validate();
+
+        return frame;
+    }
+
+    validate() {
+        moment.suppressDeprecationWarnings = true;
+
+        if(!moment(this._start).isValid())
+            throw `Error: Start date is not in a valid ISO date format!`;
+
+        if(this._stop && !moment(this._stop).isValid())
+            throw `Error: Stop date is not in a valid ISO date format!`;
+
+        moment.suppressDeprecationWarnings = false;
+    }
+
+    _getCurrentDate() {
+        if(this.timezone)
+            return moment().tz(this.timezone).format();
+
+        return moment();
+    }
+
+    get duration() {
+        return moment(this.stop).diff(this.start) / 1000;
+    }
+
+    get date() {
+        return this.start;
+    }
+
+    get start() {
+        return this.timezone ? moment(this._start).tz(this.timezone) : moment(this._start);
+    }
+
+    get stop() {
+        return this.timezone ? moment(this._stop).tz(this.timezone) : (this._stop ? moment(this._stop) : false );
+    }
+
+    /**
+     * generate a unique id
+     * @returns {number}
+     */
+    static generateId() {
+        return hashids.encode(new Date().getTime());
+    }
+}
+
+module.exports = baseFrame;
--- src/models/frame.js
+++ src/models/frame.js
@@ -1,44 +1,8 @@
 const fs = require('fs');
 const path = require('path');
-const moment = require('moment-timezone');
-const Hashids = require('hashids');
-const hashids = new Hashids();
+const BaseFrame = require('./baseFrame.js');
 
-class frame {
-    /**
-     * constructor.
-     * @param config
-     * @param id
-     * @param type
-     */
-    constructor(config, id, type) {
-        this.config = config;
-        this.project = config.get('project');
-        this.resource = {id, type};
-
-        if(typeof id === 'string' || id instanceof String)
-            this.resource.new = true;
-
-        this.id = frame.generateId();
-        this._start = false;
-        this._stop = false;
-        this.timezone = config.get('timezone');
-        this.notes = [];
-    }
-
-    static fromJson(config, json) {
-        let frame = new this(config, json.resource.id, json.resource.type);
-        frame.project = json.project;
-        frame.id = json.id;
-        frame._start = json.start;
-        frame._stop = json.stop;
-        frame.notes = json.notes;
-        frame.timezone = json.timezone;
-        frame.validate();
-
-        return frame;
-    }
-
+class frame extends BaseFrame {
     static fromFile(config, file) {
         return frame.fromJson(config, JSON.parse(fs.readFileSync(file)));
     }
@@ -55,25 +19,6 @@ class frame {
         this.write();
 
         return this;
-    }
-
-    validate() {
-        moment.suppressDeprecationWarnings = true;
-
-        if(!moment(this._start).isValid())
-            throw `Error: Start date is not in a valid ISO date format!`;
-
-        if(this._stop && !moment(this._stop).isValid())
-            throw `Error: Stop date is not in a valid ISO date format!`;
-
-        moment.suppressDeprecationWarnings = false;
-    }
-
-    _getCurrentDate() {
-        if(this.timezone)
-            return moment().tz(this.timezone).format();
-
-        return moment();
     }
 
     /**
@@ -101,30 +46,6 @@ class frame {
 
     get file() {
         return path.join(this.config.frameDir, this.id + '.json');
-    }
-
-    get duration() {
-        return moment(this.stop).diff(this.start) / 1000;
-    }
-
-    get date() {
-       return this.start;
-    }
-
-    get start() {
-        return this.timezone ? moment(this._start).tz(this.timezone) : moment(this._start);
-    }
-
-    get stop() {
-        return this.timezone ? moment(this._stop).tz(this.timezone) : (this._stop ? moment(this._stop) : false );
-    }
-
-    /**
-     * generate a unique id
-     * @returns {number}
-     */
-    static generateId() {
-        return hashids.encode(new Date().getTime());
     }
 }
 
